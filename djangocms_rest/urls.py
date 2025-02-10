7e73dc1141d5048a5a1a85844a9a79293f061499
Marc Widmer <marc@koga.cx>
2025-02-10 11:19:05 +0100

    refactor urls and remove cms from path
    should be added in parent/global urlpatterns

--- djangocms_rest/urls.py
+++ djangocms_rest/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from djangocms_rest.views import LanguageListView, PageDetailView, PageTreeListView, PlaceholderDetailView
 
 urlpatterns = [
-    path("languages/", LanguageListView.as_view(), name="cms-language-list"),
-    path("<slug:language>/pages-tree/", PageTreeListView.as_view(), name="cms-page-tree-list"),
-    path("<slug:language>/pages-root/", PageDetailView.as_view(), name="cms-page-root"),
-    path("<slug:language>/pages/<path:path>/", PageDetailView.as_view(), name="cms-page-detail"),
+    path("languages/", LanguageListView.as_view(), name="language-list"),
+    path("<slug:language>/pages-tree/", PageTreeListView.as_view(), name="page-tree-list"),
+    path("<slug:language>/pages-root/", PageDetailView.as_view(), name="page-root"),
+    path("<slug:language>/pages/<path:path>/", PageDetailView.as_view(), name="page-detail"),
     path(
         "<slug:language>/placeholders/<int:content_type_id>/<int:object_id>/<str:slot>/",
         PlaceholderDetailView.as_view(),
